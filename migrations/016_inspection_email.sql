60f6c3b3fb41828be305d3421ca7f9490b808ac8
leeshaunr-ops <leeshaunr@gmail.com>
2026-09-11 15:36:12 -0400

    Store inspection recipient and separate email delivery status

---
+++ migrations/016_inspection_email.sql
@@ -0,0 +1,2 @@
+ALTER TABLE inspections ADD COLUMN report_email TEXT NOT NULL DEFAULT '';
+CREATE TABLE inspection_email_delivery(inspection_id TEXT PRIMARY KEY REFERENCES inspections(id),email_status TEXT NOT NULL DEFAULT 'not_requested',email_attempted_at TEXT);
